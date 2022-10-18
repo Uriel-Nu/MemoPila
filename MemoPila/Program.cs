bc026f8759f349f3b38a72fdd875423114360725
UsuarioUsuario@DESKTOP-2NT6HAM <urielalberto75@hotmail.com>
2022-10-18 15:12:49 -0300

    Agregar archivos de proyecto.

---
+++ MemoPila/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoPila
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// Pila : Estructura Lifo ( El ultimo en entrar , el primero en salir
+            string palabra;
+            Stack colaPalabras = new Stack();
+            Console.WriteLine("Ingrese la  palabra");
+            palabra = Console.ReadLine();
+            while (palabra != "")
+            {
+                colaPalabras.Push(palabra);
+                Console.WriteLine("Ingrese la próxima palabra");
+                palabra = Console.ReadLine();
+            }
+            while (colaPalabras.Count > 0)
+            {
+                Console.WriteLine((string)colaPalabras.Pop());
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MemoPila/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("MemoPila")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MemoPila")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("0f428332-1a23-43a5-860a-d0c8b25c86ab")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
